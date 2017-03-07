7be18e5a96399454c97164670f91692a76c7ea14
Chiara DeLucia <chiaramdelucia@gmail.com>
2017-03-07 12:05:18 -0500

    Adding express server and routing path.

---
+++ server.js
@@ -0,0 +1,36 @@
+//Dependencies
+// =============================================================
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+//Initialize Express and local host
+// =============================================================
+var app = express();
+const PORT = process.env.PORT || 3000;
+
+// Sets up the Express app to handle data parsing
+// =============================================================
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+app.use(express.static(__dirname));
+
+//Routes
+// =============================================================
+app.get('/', function (req, res){
+	res.sendFile(path.join(__dirname, '/portfolio.html'));
+
+});
+
+// app.get('/', function (req, res){
+// 	res.sendFile(path.join(__dirname, '/../public/home.html'));
+
+// });
+
+
+//Listen on port 3000
+// =============================================================
+app.listen(PORT, function(){
+	console.log('Listening on Port ' + PORT);
+});
